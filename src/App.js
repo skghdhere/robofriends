d40ac256326e9a55d022b1e8b71e09c7ea872c99
SKGHD <sangramgorai@gmail.com>
2020-11-18 02:26:59 +0530

    till 13

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchBox from "./SearchBox";
 
 import { robots } from "./robots";
 
-
+import './App.css';
 
 class App extends Component {
     constructor() {
@@ -26,7 +26,7 @@ class App extends Component {
         );
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange }/>
                 <CardList robots={filteredRobots} />
             </div>
--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@
 
 const Card = (props) => {
     const { id, name, email } = props;
-    let src = `https://robohash.org/${id}`
+    let src = `https://robohash.org/${id}?size=200x200`
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 wr">        
             <img src={ src} alt='robots' />

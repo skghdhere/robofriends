b280dd7f26302028751ab913ae8ddebb7ec86844
SKGHD <sangramgorai@gmail.com>
2020-11-18 17:52:13 +0530

    till 16

---
+++ src/components/Scroll.js
@@ -0,0 +1,19 @@
+const Scroll = (props) => {
+    return (
+        <div
+            style={
+                {
+                    overflowY: 'scroll',
+                    
+                    height: '100vh'
+                }
+            }
+        >
+            {props.children}
+        </div>
+    )
+}
+
+
+
+export default Scroll;

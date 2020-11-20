84b0bb35767f724393b8d55ac13bc04065a2cc2c
SKGHD <sangramgorai@gmail.com>
2020-11-21 03:35:31 +0530

    till 20

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,28 +4,44 @@ import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 
 import "./App.css";
+import { setSearchField } from "../actions";
+import { connect } from "react-redux";
+
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchField: "",
     };
   }
 
   componentDidMount() {
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ robots: users }));
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
-  };
+
 
   render() {
-    const { robots, searchField } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter((robot) =>
       robot.name.toLowerCase().includes(searchField.toLowerCase())
     );
@@ -36,7 +52,7 @@ class App extends Component {
       return (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
@@ -46,4 +62,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import { createStore, applyMiddleware } from "redux";
+import { searchRobots } from "./reducers";
 import "./index.css";
 
 import App from "./containers/App";
 
-import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
+import { createLogger } from "redux-logger";
+
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
+
 ReactDOM.render(
-  <React.StrictMode>
+
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+
+  ,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
